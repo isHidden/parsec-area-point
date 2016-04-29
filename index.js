b0be00d7739f3a2e681ae001ed704b63980ebd9e
isHidden <isHidden@hotmail.com>
2016-04-29 19:13:21 +0800

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express=require("express");
 var context="node_modules/parsec-area-point/";
 module.exports=function (AV,app,city) {
     var area_router=require("./router/areaPoint")(AV);
-    var area_cloud=require("./cloud/AreaPoint");
+    var area_cloud=require("./cloud/AreaPoint")(AV);
     
     app.use(express.static(context+"public"));
     app.use('/213123716238/js',express.static(context+"baidumap/"+city));
