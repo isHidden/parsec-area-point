61d7fe72cde5dbf9c5b36c2f0ea340a8363f3bc5
isHidden <isHidden@hotmail.com>
2016-05-03 14:22:05 +0800

    新增城市武汉

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 /**
  * Created by Miku on 16/4/29.
  */
-var express=require("express");
+var express = require("express");
+var _ = require("underscore");
+var context = "node_modules/parsec-area-point/";
+module.exports = function (options) {
+    options = _.extend({
+        AV: null,
+        app: null,
+        city: null,
+        coverId: null,
+        adminPath: "/213123716238"
+    }, options);
 
-var context="node_modules/parsec-area-point/";
-module.exports=function (AV,app,city) {
-    var area_router=require("./router/areaPoint")(AV);
-    var area_cloud=require("./cloud/AreaPoint")(AV);
-    
-    app.use(express.static(context+"public"));
-    app.use('/213123716238/js',express.static(context+"baidumap/"+city));
-    app.use('/mobile/js',express.static(context+"baidumap/"+city));
+    var app = options.app;
+    var area_router = require("./router/areaPoint")(options.AV);
+    var area_cloud = require("./cloud/AreaPoint")(options.AV);
+
+    app.use(express.static(context + "public"));
+    app.use(options.adminPath, express.static(context + "admin-public"));
+    app.use(options.adminPath + '/js', express.static(context + "baidumap/" + options.city));
+    app.use('/mobile/js', express.static(context + "baidumap/" + options.city));
     app.use(area_router);
     app.use(area_cloud);
 };
